aeb7a9c8206274d621199efb6b83e60e194b9bd5
md-golam-rabbani <rabbani.cse.69@gmail.com>
2026-01-29 21:50:39 +0600

    refactor: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for all origins, methods, and headers
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   // Apply ValidationPipe globally for incoming request DTOs
   // app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
